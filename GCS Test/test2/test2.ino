023cda2697cd7be2499658ef426307a0a68007d4
Denzel George <39189220+DenzelGeorge@users.noreply.github.com>
2021-01-16 01:35:29 +0530

    Index value correction

--- GCS Test/test2/test2.ino
+++ GCS Test/test2/test2.ino
@@ -6,6 +6,6 @@ void loop(){
   String AzEl = "213.9$-37.7";
   int ind = AzEl.indexOf("$");
   String Az = AzEl.substring(0,ind);
-  String El = AzEl.substring(ind);
+  String El = AzEl.substring(ind + 1);
   Serial.println("AZ: " + Az + " EL: " + El);
 }
