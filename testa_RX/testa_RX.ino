c8f654e6867042305307ab97103f2187ae7d2c3e
Denzel George <39189220+DenzelGeorge@users.noreply.github.com>
2021-02-16 17:26:08 +0530

    test codes

--- testa_RX/testa_RX.ino
+++ testa_RX/testa_RX.ino
@@ -1,14 +1,23 @@
 #include <RadioLib.h>
+#include <Adafruit_GFX.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
 
 //Define pins for LoRa interface
 #define NSS 4
 #define DIO1 5
 #define NRST 3
 #define BUSY 2
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 
 //Initializing Module
 SX1262 radio = new Module(NSS,DIO1,NRST,BUSY);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+//CAllSign 
+char callsign[] = "UNITYSat";
 
 void setup() {
   // put your setup code here, to run once:
@@ -23,6 +32,12 @@ void setup() {
     Serial.println(state);
     while (true);
     }
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+  Serial.println("SSD1306 allocation failed");
+  for(;;); // Don't proceed, loop forever
+    }
+  Serial.println("SSD1306 allocation failed");
+  
 }
 
 void loop() {
@@ -64,7 +79,17 @@ void loop() {
     // some other error occurred
     Serial.print(F("failed, code "));
     Serial.println(state);
+    //delay(2000);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE); 
 
+  display.setCursor(0,10);   
+  display.println("UNITYSAT Test2");
+  display.setCursor(0,32);   
+  display.print("RSSI: ");
+  display.println(radio.getRSSI());
+  display.display();
   }
   
 }
